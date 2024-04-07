2a3eb774461f68f8d7ad5c34e9dbf8757430bb97
Darmohrai <darmovad.com>
2024-04-08 00:27:58 +0300

    Create method pushFront in class DoubleLinkedList

--- DoubleLinkedList.h
+++ DoubleLinkedList.h
@@ -9,21 +9,45 @@
 #include <memory>
 
 template<typename T>
-class DoubleLinkedList{
+class DoubleLinkedList {
 private:
-    struct Node{
+    struct Node {
         T data;
         std::shared_ptr<Node> next;
         std::weak_ptr<Node> previous;
 
-        Node(T data) : data{data}{};
+        Node(T data) : data{data} {};
     };
 
-    std::unique_ptr<Node> head = nullptr;
-    std::unique_ptr<Node> tail = nullptr;
+    std::shared_ptr<Node> head = nullptr;
+    std::shared_ptr<Node> tail = nullptr;
     size_t size = 0;
 public:
     DoubleLinkedList() = default;
+
+    void pushFront(T value) {
+        std::shared_ptr<Node> newNode = std::make_shared<Node>(value);
+        if (head == nullptr) {
+            head = tail = newNode;
+            head->next = nullptr;
+            size++;
+            return;
+        }
+        newNode->next = head;
+        head->previous = newNode;
+        head = newNode;
+        size++;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, DoubleLinkedList &other){
+        Node *current = other.head.get();
+        while(current != nullptr){
+            os << current->data << " ";
+            current = current->next.get();
+        }
+        os << std::endl;
+        return os;
+    }
 
     ~DoubleLinkedList() = default;
 };
--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "SinglyLinkedList.h"
 #include "DoubleLinkedList.h"
 
-void Show(SinglyLinkedList<int> obj) {
-    obj.show();
-}
-
 int main() {
     // code verification
-
+    DoubleLinkedList<int> obj;
+    obj.pushFront(3);
+    obj.pushFront(8);
+    obj.pushFront(1);
+    std::cout << obj;
     return 0;
 }
